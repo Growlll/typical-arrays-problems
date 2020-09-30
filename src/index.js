73f7ee47a16296fdda7012c9e596d7692d143edd
Vitaliy <vetal.morenko@mail.ru>
2020-09-30 22:30:08 +0300

    feat: add all functions for finding numbers min, max, average

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
 exports.min = function min (array) {
-  return 0;
+    const [...newArr] = array || []
+    return (newArr !== 'undefined' && newArr.length !== 0) ? Math.min(...newArr) : 0
 }
 
 exports.max = function max (array) {
-  return 0;
+    const [...newArr] = array || []
+    return (newArr !== 'undefined' && newArr.length !== 0) ? Math.max(...newArr) : 0
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    const [...newArr] = array || []
+    if(newArr.length === 0 && newArr !== 'undefined') return 0
+    const sum = newArr.reduce((acc, curr) => acc + curr)
+    return (sum / array.length)
 }
